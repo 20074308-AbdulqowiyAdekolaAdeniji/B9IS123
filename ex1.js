8fcf2da71a7b1cba01ca5eba1fbcaaa8fca7518e
20074308-AbdulqowiyAdekolaAdeniji <20074308@mydbs.ie>
2025-09-30 14:04:56 +0100

    Updating Button Functionality

--- ex1.js
+++ ex1.js
@@ -1,2 +1,17 @@
-//This script is for the baggage weight exercise
-//This is my attempt to create an input element and make it functional
+// This script is for the baggage weight exercise
+// Function to check baggage weight
+const customerMessage = () => {
+    const value = document.getElementById("weight").value; // get input value from user
+    const weight = parseInt(value); // convert to integer insantly
+    
+    if (isNaN(weight)) {
+      alert("Please enter a valid number");  // Checks if a valid number has been entered
+      return;
+    }
+
+    if (weight <= 15) {
+      alert(`The baggage weights ${weight}, it is acceptable`);
+    } else {
+      alert(`The baggage is overweight`);
+    }
+  }
