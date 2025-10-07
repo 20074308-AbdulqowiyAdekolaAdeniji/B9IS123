cbb1485610b31dc5c12c013f1b2cd315913d47ae
20074308-AbdulqowiyAdekolaAdeniji <20074308@mydbs.ie>
2025-10-07 19:19:24 +0100

    Added Exercise 5

---
+++ ex5.js
@@ -0,0 +1,59 @@
+//   let parse=(i)=>{
+//       o=parseInt(i)
+//       if (isNaN(o))
+//         throw("NaN");
+//       return o;
+//     }
+    
+//Create a modified version of ex4 that prepends "corrupt" where an error occurs
+
+//Examples ()
+// 2 3 5 67
+// corrupt : 2 3 5 67
+
+//Actually this one will not result in an error unless you throw one as in parse(i) above:
+// 3 5 hello: 1 2 3 4 5 6 7 8 9
+// corrupt : 3 5 hello: 1 2 3 4 5 6 7 8 9
+
+//Create a further version that will output the answer omitting corrupted values
+//or "corrupt" if it cannot be resolved
+
+// 3 5 hello: 1 2 3 4 5 6 7 8 9
+// 23 : 3 5 hello: 1 2 3 4 5 6 7 8 9
+
+//this cannot be fixed - actually this is the only one that will throw errors
+// 2 3 5 67
+// corrupt : 2 3 5 67
+
+// Function to calculate Sum of Multiples for the arrays
+let eulerArrayC5 = (arr, list) => {
+
+    let sum = list
+    .filter(n => arr.some(m => n % m === 0))
+    .reduce((a, b) => a + b, 0);
+
+    return sum
+}
+
+// const questionString = "3 5 : 1 2 3 4 5 6 7 8 9"
+
+
+// Function to take "3 5 : 1 2 3 4 5 6 7 8 9" and calculate
+const ex5result = (questionString) =>{
+    const multipleString = questionString.split(":")[0].split('  ').toString('').trimEnd().split(' ').map(n=>parseInt(n));
+    const listString = questionString.split(":")[1].split('  ').toString('').trimStart().split(' ').map(n=>parseInt(n));
+
+    const result = eulerArrayC5 (multipleString, listString)
+
+    return result
+}
+
+//console.log(ex4result('3 5 : 1 2 3 4 5 6 7 8 9'))
+
+//Callback function for onClick function
+const ex5Display = () => {
+    const a = document.getElementById("strList").value
+    console.log(a)
+        const answer = ex5result(a)
+        alert(`The answer to the sum of multiples is ${answer}`)
+}
